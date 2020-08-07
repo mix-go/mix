e3b600ed7a6960d6f83aad7bf8b54c5194afb274
LIU JIAN <onanying@126.com>
2020-08-07 18:37:58 +0800

    feat: add Daemon

---
+++ src/console/process/daemon.go
@@ -0,0 +1,33 @@
+package process
+
+import (
+    "errors"
+    "os"
+    "os/exec"
+    "runtime"
+)
+
+// 使当前进程蜕变为一个守护进程
+func Daemon() {
+    ok := false
+    switch runtime.GOOS {
+    case "darwin", "linux":
+        ok = true
+    case "windows":
+        ok = false
+    default:
+        ok = true
+    }
+
+    if !ok {
+        panic(errors.New("The current OS does not support background execution"))
+    }
+
+    if os.Getppid() != 1 {
+        cmd := exec.Command(os.Args[0], os.Args[1:]...)
+        if err := cmd.Start(); err != nil {
+            panic(err)
+        }
+        os.Exit(0)
+    }
+}
