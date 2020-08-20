8dc1a78a81a3617fd98f939f45051c4e1cb3fdc8
LIU JIAN <onanying@126.com>
2020-08-20 10:16:55 +0800

    feat: add gid check

--- src/console/process/daemon.go
+++ src/console/process/daemon.go
@@ -1,11 +1,13 @@
 package process
 
 import (
+    "bytes"
     "errors"
     "github.com/mix-go/console"
     "os"
     "os/exec"
     "runtime"
+    "strconv"
 )
 
 // 使当前进程蜕变为一个守护进程
@@ -24,6 +26,10 @@ func Daemon() {
         panic(console.UnsupportError(errors.New("The current OS does not support background execution")))
     }
 
+    if getgid() != 1 {
+        panic(errors.New("Daemon() can only be used in Main Goroutine"))
+    }
+
     if os.Getppid() != 1 {
         cmd := exec.Command(os.Args[0], os.Args[1:]...)
         if err := cmd.Start(); err != nil {
@@ -31,4 +37,13 @@ func Daemon() {
         }
         os.Exit(0)
     }
+}
+
+func getgid() uint64 {
+    b := make([]byte, 64)
+    runtime.Stack(b, false)
+    b = bytes.TrimPrefix(b, []byte("goroutine "))
+    b = b[:bytes.IndexByte(b, ' ')]
+    n, _ := strconv.ParseUint(string(b), 10, 64)
+    return n
 }
