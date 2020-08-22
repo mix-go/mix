78050c066de5179d37dd868589a56113130d691e
LIU JIAN <onanying@126.com>
2020-08-22 15:36:17 +0800

    feat: 取值方式统一

--- src/console/flag/flag.go
+++ src/console/flag/flag.go
@@ -5,7 +5,79 @@ import (
     "strconv"
 )
 
-func String(name string, value string) string {
+type flagValue struct {
+    v string
+}
+
+func (t *flagValue) String(val ...string) string {
+    d := ""
+    if len(val) >= 1 {
+        d = val[0]
+    }
+
+    if t.v == "" {
+        return d
+    }
+
+    return t.v
+}
+
+func (t *flagValue) Bool(val ...bool) bool {
+    d := false
+    if len(val) >= 1 {
+        d = val[0]
+    }
+
+    switch t.v {
+    case "":
+        return d
+    case "0", "false":
+        return false
+    default:
+        return true
+    }
+}
+
+func (t *flagValue) Int64(val ...int64) int64 {
+    d := int64(0)
+    if len(val) >= 1 {
+        d = val[0]
+    }
+
+    if t.v == "" {
+        return d
+    }
+
+    v, _ := strconv.ParseInt(t.v, 10, 64)
+    return v
+}
+
+func (t *flagValue) Float64(val ...float64) float64 {
+
+    d := float64(0)
+    if len(val) >= 1 {
+        d = val[0]
+    }
+
+    if t.v == "" {
+        return d
+    }
+
+    v, _ := strconv.ParseFloat(t.v, 64)
+    return v
+}
+
+func Match(names ...string) *flagValue {
+    for _, name := range names {
+        v := value(name)
+        if v != "" {
+            return &flagValue{v}
+        }
+    }
+    return &flagValue{}
+}
+
+func value(name string) string {
     key := ""
     if len(name) == 1 {
         key = fmt.Sprintf("-%s", name)
@@ -15,85 +87,5 @@ func String(name string, value string) string {
     if v, ok := Options()[key]; ok {
         return v
     }
-    return value
-}
-
-func StringMatch(names []string, value string) string {
-    for _, name := range names {
-        v := String(name, value)
-        if v != value {
-            return v
-        }
-    }
-    return value
-}
-
-func Bool(name string, value bool) bool {
-    key := ""
-    if len(name) == 1 {
-        key = fmt.Sprintf("-%s", name)
-    } else {
-        key = fmt.Sprintf("--%s", name)
-    }
-    if v, ok := Options()[key]; ok {
-        if v == "false" {
-            return false
-        }
-        return true
-    }
-    return value
-}
-
-func BoolMatch(names []string, value bool) bool {
-    for _, name := range names {
-        v := Bool(name, value)
-        if v != value {
-            return v
-        }
-    }
-    return value
-}
-
-func Int64(name string, value int64) int64 {
-    v := String(name, "")
-    if v == "" {
-        return value
-    }
-    i, err := strconv.ParseInt(v, 10, 64)
-    if err != nil {
-        return value
-    }
-    return i
-}
-
-func Int64Match(names []string, value int64) int64 {
-    for _, name := range names {
-        v := Int64(name, value)
-        if v != value {
-            return v
-        }
-    }
-    return value
-}
-
-func Float64(name string, value float64) float64 {
-    v := String(name, "")
-    if v == "" {
-        return value
-    }
-    f, err := strconv.ParseFloat(v, 64)
-    if err != nil {
-        return value
-    }
-    return f
-}
-
-func Float64Match(names []string, value float64) float64 {
-    for _, name := range names {
-        v := Float64(name, value)
-        if v != value {
-            return v
-        }
-    }
-    return value
+    return key
 }
