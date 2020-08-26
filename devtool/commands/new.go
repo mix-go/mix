2bb08c44944a458e5918096614fae663d568f7bd
LIU JIAN <onanying@126.com>
2020-08-26 10:44:17 +0800

    improve: alter note

--- devtool/commands/new.go
+++ devtool/commands/new.go
@@ -42,7 +42,7 @@ func (t *NewCommand) NewProject(name, typ string) {
 
     sDir := fmt.Sprintf("%s/pkg/mod/github.com/mix-go/mix-%s-skeleton@%s", os.Getenv("GOPATH"), typ, ver)
     if _, err := os.Stat(sDir); err != nil {
-        fmt.Println(fmt.Sprintf("Skeleton '%s' not found, exec 'go get -u github.com/mix-go/mix-%s-skeleton@%s'", typ, typ, ver))
+        fmt.Println(fmt.Sprintf("Skeleton '%s' local not found, exec 'go get -u github.com/mix-go/mix-%s-skeleton@%s', please wait ...", typ, typ, ver))
         cmd := exec.Command("go", "get", "-u", fmt.Sprintf("github.com/mix-go/mix-%s-skeleton@%s", typ, ver))
         err = cmd.Run()
         if err != nil {
