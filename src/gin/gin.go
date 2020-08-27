4395e55f8c81325b2a559ea05c46142a465f9401
LIU JIAN <onanying@126.com>
2020-08-27 20:14:52 +0800

    improve: update gin

--- src/gin/gin.go
+++ src/gin/gin.go
@@ -2,15 +2,23 @@ package gin
 
 import "github.com/gin-gonic/gin"
 
-type IRouter interface {
-    gin.IRouter
-    Run() error
-}
-
-func New(definitions ...func(router *gin.Engine)) IRouter {
+func New(definitions ...func(router *gin.Engine)) *gin.Engine {
     e := gin.New()
     for _, d := range definitions {
         d(e)
     }
     return e
+}
+
+const (
+    // DebugMode indicates gin mode is debug.
+    DebugMode = "debug"
+    // ReleaseMode indicates gin mode is release.
+    ReleaseMode = "release"
+    // TestMode indicates gin mode is test.
+    TestMode = "test"
+)
+
+func SetMode(value string) {
+    gin.SetMode(value)
 }
