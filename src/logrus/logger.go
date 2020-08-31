b53cc81e7685862eb0b5f5cbafc0a1efba7fe7cc
LIU JIAN <onanying@126.com>
2020-08-31 17:37:45 +0800

    feat: add millisecond

--- src/logrus/logger.go
+++ src/logrus/logger.go
@@ -25,7 +25,7 @@ func NewLogger() *Logger {
 
     formatter := new(l.TextFormatter)
     formatter.FullTimestamp = true
-    formatter.TimestampFormat = "2006-01-02 15:04:05"
+    formatter.TimestampFormat = "2006-01-02 15:04:05.000"
     formatter.DisableQuote = true // 不转义换行符，为了保存错误堆栈到日志文件
     formatter.CallerPrettyfier = func(frame *runtime.Frame) (function string, file string) {
         return "", fmt.Sprintf("%s:%d", filepath.Base(frame.File), frame.Line)
