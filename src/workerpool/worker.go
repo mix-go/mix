900e07743532757b341491cda122e96337f58eb2
LIU JIAN <onanying@126.com>
2020-09-18 17:56:02 +0800

    feat: remove Error func

--- src/workerpool/worker.go
+++ src/workerpool/worker.go
@@ -5,32 +5,28 @@ import (
 )
 
 type Handler func(data interface{})
-type ErrorHandler func(err interface{})
 
 type Worker interface {
-    Init(WorkerID int, workerPool chan JobQueue, wg *sync.WaitGroup, handler Handler, errorHandler ErrorHandler)
+    Init(WorkerID int, workerPool chan JobQueue, wg *sync.WaitGroup, handler Handler)
     Run()
     Stop()
     Do(data interface{})
-    Error(err interface{})
 }
 
 type WorkerTrait struct {
-    WorkerID     int
-    workerPool   chan JobQueue
-    wg           *sync.WaitGroup
-    handler      Handler
-    errorHandler ErrorHandler
-    jobChan      JobQueue
-    quit         chan bool
+    WorkerID   int
+    workerPool chan JobQueue
+    wg         *sync.WaitGroup
+    handler    Handler
+    jobChan    JobQueue
+    quit       chan bool
 }
 
-func (t *WorkerTrait) Init(WorkerID int, workerPool chan JobQueue, wg *sync.WaitGroup, handler Handler, errorHandler ErrorHandler) {
+func (t *WorkerTrait) Init(WorkerID int, workerPool chan JobQueue, wg *sync.WaitGroup, handler Handler) {
     t.WorkerID = WorkerID
     t.workerPool = workerPool
     t.wg = wg
     t.handler = handler
-    t.errorHandler = errorHandler
     t.jobChan = make(chan interface{})
     t.quit = make(chan bool)
 }
@@ -39,11 +35,6 @@ func (t *WorkerTrait) Run() {
     t.wg.Add(1)
     go func() {
         defer t.wg.Done()
-        defer func() {
-            if err := recover(); err != nil {
-                t.errorHandler(err)
-            }
-        }()
         t.workerPool <- t.jobChan
         for {
             select {
