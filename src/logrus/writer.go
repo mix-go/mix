94dea9d19905524a307a54e5c3f1e05bd41340b0
LIU JIAN <onanying@126.com>
2020-10-28 18:28:58 +0800

    feat: 修改 count, size == 0 时不轮转

--- src/logrus/writer.go
+++ src/logrus/writer.go
@@ -4,14 +4,20 @@ import (
     rotatelogs "github.com/lestrrat-go/file-rotatelogs"
 )
 
+// 创建文件日志处理
+// count, size == 0 时不轮转
 func NewFileWriter(filename string, count uint, size int64) *rotatelogs.RotateLogs {
-    writer, err := rotatelogs.New(
-        filename+".%Y%m%d",
+    options := []rotatelogs.Option{
         rotatelogs.WithLinkName(filename),
         rotatelogs.WithMaxAge(-1),
-        rotatelogs.WithRotationCount(count),
-        rotatelogs.WithRotationSize(size),
-    )
+    }
+    if count > 0 {
+        options = append(options, rotatelogs.WithRotationCount(count))
+    }
+    if size > 0 {
+        options = append(options, rotatelogs.WithRotationSize(size))
+    }
+    writer, err := rotatelogs.New(filename+".%Y%m%d", options...)
     if err != nil {
         panic(err)
     }
