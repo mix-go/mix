1f6504cbc292f3aeacb4dd6dc6df27bf66b285e0
LIU JIAN <onanying@126.com>
2020-11-10 18:51:44 +0800

    feat: 格式化缩进，为了满足 gofmt 的检测

--- load.go
+++ load.go
@@ -4,5 +4,5 @@ import "github.com/joho/godotenv"
 
 // Load will read your env file(s) and load them into ENV for this process.
 func Load(filenames ...string) (err error) {
-    return godotenv.Load(filenames...)
+	return godotenv.Load(filenames...)
 }
