af9e1cb8751d18b5b9120fba751c1ac9e2f35fde
LIU JIAN <onanying@126.com>
2020-11-17 15:49:20 +0800

    feat: update Daemon

--- src/console/process/daemon.go
+++ src/console/process/daemon.go
@@ -1,50 +1,50 @@
 package process
 
 import (
-	"bytes"
-	"errors"
-	"github.com/mix-go/console"
-	"os"
-	"os/exec"
-	"runtime"
-	"strconv"
+    "bytes"
+    "fmt"
+    "github.com/mix-go/console"
+    "os"
+    "os/exec"
+    "runtime"
+    "strconv"
 )
 
 // Daemon 使当前进程蜕变为一个守护进程
 func Daemon() {
-	var ok bool
-	switch runtime.GOOS {
-	case "darwin", "linux":
-		ok = true
-	case "windows":
-		ok = false
-	default:
-		ok = true
-	}
-	if !ok {
-		panic(console.NewUnsupportError(errors.New("Error: the current OS does not support background execution")))
-	}
+    var ok bool
+    switch runtime.GOOS {
+    case "darwin", "linux":
+        ok = true
+    case "windows":
+        ok = false
+    default:
+        ok = true
+    }
+    if !ok {
+        panic(console.NewUnsupportError(fmt.Errorf("error: the current OS does not support background execution")))
+    }
 
-	if getgid() != 1 {
-		panic(errors.New("Daemon() can only be used in Main Goroutine"))
-	}
+    if getgid() != 1 {
+        panic(fmt.Errorf("error: Daemon() can only be used in main goroutine"))
+    }
 
-	// Getppid 父进程ID: 当父进程已经结束，在Unix中返回的ID是初始进程(1)中的一个，在Windows中仍然是同一个进程ID，该进程ID有可能已经被其他进程占用
-	if os.Getppid() != 1 {
-		cmd := exec.Command(os.Args[0], os.Args[1:]...)
-		if err := cmd.Start(); err != nil {
-			panic(err)
-		}
-		os.Exit(0)
-	}
+    // Getppid 父进程ID: 当父进程已经结束，在Unix中返回的ID是初始进程(1)，在Windows中仍然是同一个进程ID，该进程ID有可能已经被其他进程占用
+    if os.Getppid() != 1 {
+        cmd := exec.Command(os.Args[0], os.Args[1:]...)
+        if err := cmd.Start(); err != nil {
+            panic(err)
+        }
+        os.Exit(0)
+    }
 }
 
 // 获取协程id
 func getgid() uint64 {
-	b := make([]byte, 64)
-	runtime.Stack(b, false)
-	b = bytes.TrimPrefix(b, []byte("goroutine "))
-	b = b[:bytes.IndexByte(b, ' ')]
-	n, _ := strconv.ParseUint(string(b), 10, 64)
-	return n
+    b := make([]byte, 64)
+    runtime.Stack(b, false)
+    b = bytes.TrimPrefix(b, []byte("goroutine "))
+    b = b[:bytes.IndexByte(b, ' ')]
+    n, _ := strconv.ParseUint(string(b), 10, 64)
+    return n
 }
