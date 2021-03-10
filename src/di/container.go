8b66caaf015914c61a26c102bd9808edad140f0f
LIU JIAN <onanying@126.com>
2021-03-10 17:24:58 +0800

    feat: up

--- src/di/container.go
+++ src/di/container.go
@@ -1,97 +1,97 @@
 package di
 
 import (
-    "fmt"
-    "reflect"
-    "sync"
+	"fmt"
+	"reflect"
+	"sync"
 )
 
 var iContainer *container
 
 func init() {
-    iContainer = New()
+	iContainer = New()
 }
 
 // New
 func New() *container {
-    return &container{}
+	return &container{}
 }
 
 // Container
 func Container() *container {
-    return iContainer
+	return iContainer
 }
 
 // Provide
 func Provide(objects ...*Object) error {
-    return iContainer.Provide(objects...)
+	return iContainer.Provide(objects...)
 }
 
 // Find
 func Find(name string, ptr interface{}) error {
-    return iContainer.Find(name, ptr)
+	return iContainer.Find(name, ptr)
 }
 
 // container
 type container struct {
-    Objects     []*Object
-    tidyObjects sync.Map
-    instances   sync.Map
+	Objects     []*Object
+	tidyObjects sync.Map
+	instances   sync.Map
 }
 
 // Provide
 func (t *container) Provide(objects ...*Object) error {
-    t.tidyObjects = sync.Map{}
-    for _, o := range objects {
-        if _, ok := t.tidyObjects.Load(o.Name); ok {
-            return fmt.Errorf("error: ")
-        }
-        t.tidyObjects.Store(o.Name, o)
-    }
-    return nil
+	t.tidyObjects = sync.Map{}
+	for _, o := range objects {
+		if _, ok := t.tidyObjects.Load(o.Name); ok {
+			return fmt.Errorf("error: ")
+		}
+		t.tidyObjects.Store(o.Name, o)
+	}
+	return nil
 }
 
 // Object
 func (t *container) Object(name string) (*Object, error) {
-    v, ok := t.tidyObjects.Load(name)
-    if !ok {
-        return nil, fmt.Errorf("error: object '%s' not found", name)
-    }
-    obj := v.(*Object)
-    return obj, nil
+	v, ok := t.tidyObjects.Load(name)
+	if !ok {
+		return nil, fmt.Errorf("error: object '%s' not found", name)
+	}
+	obj := v.(*Object)
+	return obj, nil
 }
 
 // Find
 func (t *container) Find(name string, ptr interface{}) error {
-    obj, err := t.Object(name)
-    if err != nil {
-        return err
-    }
-    ptrCopy := func(to, from interface{}) {
-        reflect.ValueOf(to).Elem().Set(reflect.ValueOf(from))
-    }
-    if obj.Singleton {
-        refresher := &obj.refresher
-        if p, ok := t.instances.Load(name); ok && !refresher.status() {
-            ptrCopy(ptr, p)
-            return nil
-        }
-        v, err := obj.New()
-        if err != nil {
-            return err
-        }
-        refresher.invoke(func() {
-            t.instances.Store(name, v)
-        })
-        p, _ := t.instances.LoadOrStore(name, v) // LoadOrStore 处理并发穿透
-        ptrCopy(ptr, p)
-        return nil
-    } else {
-        v, err := obj.New()
-        if err != nil {
-            return err
-        }
-        ptrCopy(ptr, v)
-    }
-    return nil
+	obj, err := t.Object(name)
+	if err != nil {
+		return err
+	}
+	ptrCopy := func(to, from interface{}) {
+		reflect.ValueOf(to).Elem().Set(reflect.ValueOf(from))
+	}
+	if obj.Singleton {
+		refresher := &obj.refresher
+		if p, ok := t.instances.Load(name); ok && !refresher.status() {
+			ptrCopy(ptr, p)
+			return nil
+		}
+		v, err := obj.New()
+		if err != nil {
+			return err
+		}
+		refresher.invoke(func() {
+			t.instances.Store(name, v)
+		})
+		p, _ := t.instances.LoadOrStore(name, v) // LoadOrStore 处理并发穿透
+		ptrCopy(ptr, p)
+		return nil
+	} else {
+		v, err := obj.New()
+		if err != nil {
+			return err
+		}
+		ptrCopy(ptr, v)
+	}
+	return nil
 }
--- src/di/object.go
+++ src/di/object.go
@@ -1,52 +1,52 @@
 package di
 
 import (
-    "fmt"
-    "sync"
+	"fmt"
+	"sync"
 )
 
 // Object
 type Object struct {
-    Name      string
-    New       func() (interface{}, error)
-    Singleton bool
+	Name      string
+	New       func() (interface{}, error)
+	Singleton bool
 
-    refresher refresher
+	refresher refresher
 }
 
 type refresher struct {
-    mux sync.Mutex
-    val bool
+	mux sync.Mutex
+	val bool
 }
 
 func (t *refresher) on() {
-    t.val = true
+	t.val = true
 }
 
 func (t *refresher) off() {
-    t.val = false
+	t.val = false
 }
 
 func (t *refresher) status() bool {
-    return t.val
+	return t.val
 }
 
 func (t *refresher) invoke(f func()) {
-    if !t.status() {
-        return
-    }
-    t.mux.Lock()
-    defer t.mux.Unlock()
-    if t.status() {
-        f()
-        t.off()
-    }
+	if !t.status() {
+		return
+	}
+	t.mux.Lock()
+	defer t.mux.Unlock()
+	if t.status() {
+		f()
+		t.off()
+	}
 }
 
 func (t *Object) Refresh() error {
-    if !t.Singleton {
-        return fmt.Errorf("error: '%s' not a singleton, unable to refresh", t.Name)
-    }
-    t.refresher.on()
-    return nil
+	if !t.Singleton {
+		return fmt.Errorf("error: '%s' not a singleton, unable to refresh", t.Name)
+	}
+	t.refresher.on()
+	return nil
 }
