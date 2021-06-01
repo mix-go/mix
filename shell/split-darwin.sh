609ba4043199d3b3f06007cee830678ffd648e6a
liujian <onanying@126.com>
2021-06-01 15:44:21 +0800

    feat: 修改 split 脚本

--- shell/split-darwin.sh
+++ shell/split-darwin.sh
@@ -12,7 +12,7 @@ BASEPATH=$(
 REPOS=$@
 
 function split() {
-  SHA1=$(./shell/splitsh-lite --prefix=$1)
+  SHA1=$(./shell/splitsh-lite-darwin-amd64 --prefix=$1)
   git push $2 "$SHA1:refs/heads/$CURRENT_BRANCH" -f
 }
 
