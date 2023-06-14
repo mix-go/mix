93264a28c9dc082f163607f12052131350df33ec
liujian <onanying@126.com>
2023-06-14 19:05:10 +0800

    Update xutil

---
+++ src/xutil/xhttp/request_test.go
@@ -0,0 +1,26 @@
+package xhttp
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestRequest(t *testing.T) {
+	a := assert.New(t)
+
+	url := "https://github.com/"
+	resp, err := Request("GET", url)
+
+	a.Equal(resp.StatusCode, 200)
+	a.Nil(err)
+}
+
+func TestRequestError(t *testing.T) {
+	a := assert.New(t)
+
+	url := "https://aaaaa.com/"
+	resp, err := Request("GET", url)
+
+	a.Nil(resp)
+	a.NotNil(err)
+}
