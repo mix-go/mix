91baef49237ebfb9bdac15fc57050cb3002c7757
liujian <onanying@126.com>
2023-11-09 15:11:08 +0800

    Update xrpc

--- src/xrpc/generate-pb.sh
+++ src/xrpc/generate-pb.sh
@@ -3,8 +3,7 @@ set -ex
 
 BASEPATH=$(pwd)
 GOPATH=$(go env GOPATH)
-OUT=$BASEPATH
 repos=$(find . -name "*.proto")
 for FILE in $repos; do
-  protoc -I . -I `go env GOPATH`/src --go_out=paths=source_relative:. --go-grpc_out=paths=source_relative:. --grpc-gateway_out=paths=source_relative:. $FILE
+  protoc -I . -I $GOPATH/src --go_out=paths=source_relative:$BASEPATH --go-grpc_out=paths=source_relative:$BASEPATH --grpc-gateway_out=paths=source_relative:$BASEPATH $FILE
 done
